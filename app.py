b731115b40db607329876f90191337186cd89bd0
arsalananwar11 <arsalan.anwar.7777@gmail.com>
2023-04-04 00:04:23 +0530

    Added experience section

--- app.py
+++ app.py
@@ -13,5 +13,11 @@ from flask import (
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/experience")
+def experience():
+    return render_template("experience.html")
