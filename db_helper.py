ee273016fcd132c2cc1c44c3f15a28285e7f9446
arsalananwar11 <arsalan.anwar.7777@gmail.com>
2023-04-07 02:20:21 +0530

    [Feature] Projects will now fetched dynamically from DB

--- db_helper.py
+++ db_helper.py
@@ -2,12 +2,14 @@ import sqlite3
 
 class DatabaseActions:
     def __init__(self, db_name) -> None:
-        # Connect to DB and create a cursor [Production]
+        # Create a cursor [Production]
         db_connection_string = '/home/arsalananwar/Arsalan-Portfolio/' + db_name
-        self.sqliteConnection = sqlite3.connect(db_connection_string)
 
-        # Connect to DB and create a cursor [Local]
-        # self.sqliteConnection = sqlite3.connect(db_name)
+        # Create a cursor [Local]
+        # db_connection_string = db_name
+
+        # Connect to DB
+        self.sqliteConnection = sqlite3.connect(db_connection_string)
         print('[SUCCESS] DB Initialized')
     
     def add_message(self, requestor_message ):
@@ -25,8 +27,49 @@ class DatabaseActions:
             print('[ERROR] Error occurred while adding the message- ', error)
             print(f"[ERROR] Message details:\nEmail: { requestor_message[0] }\nName: { requestor_message[1] }\nSubject: { requestor_message[2] }\nMessage: { requestor_message[3] }")
 
+    def fetch_all_projects(self):
+        try:
+            self.connection_obj = self.sqliteConnection.cursor()
+            query = """SELECT * FROM PROJECTS"""
+            self.connection_obj.execute(query)
+            
+            all_projects = self.connection_obj.fetchall()
+            all_projects_list = []
+            for project in all_projects:
+                all_projects_list.append(project)
+
+            self.sqliteConnection.commit()
+            print(f"[SUCCESS] All projects successfully fetched")
+            self.connection_obj.close()
+
+            return all_projects_list
+
+        # Handle errors
+        except sqlite3.Error as error:
+            print('[ERROR] Error occurred while fetching all the projects - ', error)
+
+    def fetch_featured_projects(self):
+        try:
+            self.connection_obj = self.sqliteConnection.cursor()
+            query = """SELECT * FROM PROJECTS WHERE FEATUREDPROJECT = 1 LIMIT 4"""
+            self.connection_obj.execute(query)
+            
+            featured_projects = self.connection_obj.fetchall()
+            featured_projects_list = []
+            for project in featured_projects:
+                featured_projects_list.append(project)
+
+            self.sqliteConnection.commit()
+            print(f"[SUCCESS] Featured projects successfully fetched")
+            self.connection_obj.close()
+            return featured_projects_list
+
+        # Handle errors
+        except sqlite3.Error as error:
+            print('[ERROR] Error occurred while fetching the featured projects - ', error)
+
+    def close_db_connection(self):
         # Close DB Connection irrespective of success or failure
-        finally:
-            if self.sqliteConnection:
-                self.sqliteConnection.close()
-                print('[SUCCESS] SQLite Connection Closed')
+        if self.sqliteConnection:
+            self.sqliteConnection.close()
+            print('[SUCCESS] SQLite Connection Closed')
--- db_table_initializer.py
+++ db_table_initializer.py
@@ -2,17 +2,44 @@ import sqlite3
 
 try:
 
-	# Connect to DB and create a cursor
-	sqliteConnection = sqlite3.connect('arsalan_site_db.db')
+	db_name = 'arsalan_site_db.db'
+	
+	# Create a cursor [Production]
+	db_connection_string = '/home/arsalananwar/Arsalan-Portfolio/' + db_name
+
+	# Create a cursor [Local]
+	# db_connection_string = db_name
+	sqliteConnection = sqlite3.connect(db_connection_string)
 	cursor = sqliteConnection.cursor()
 	print('[SUCCESS] DB Initialized')
 
 	# Write a query and execute it with cursor
-	query = 'CREATE TABLE MESSAGES (Email varchar(100), Name varchar (100), Subject text, Message text)'
+	#query = 'CREATE TABLE MESSAGES (Email varchar(100), Name varchar (100), Subject text, Message text)'
+	query = 'DROP TABLE PROJECTS'
+	cursor.execute(query)
+	print('[SUCCESS] Query executed')
+	
+	query = 'CREATE TABLE PROJECTS (ProjectId int PRIMARY KEY, ProjectName text, ProjectDescription text, Category text, ProjectLink text, FeaturedProject bit, ProjectCover text)'
+	cursor.execute(query)
+	print('[SUCCESS] Query executed')
+
+	query = """INSERT INTO PROJECTS VALUES (1, 'Aqua Analytica: Water Demand Forecast Analytics', 'A Water Consumption Analytics System for Bengaluru using advanced ML techniques such as ARIMA, SARIMA & Holt-Winters for forecasting' ,'Time Series Forecasting, Data Analytics', 'https://aquaanalytica.pythonanywhere.co', 1, 'aqua.png');"""
 	cursor.execute(query)
 
-	print('[SUCCESS] Messages Table Created')
+	query = """INSERT INTO PROJECTS VALUES (2, 'Autonomous Robot Navigation using RL', 'Trained a Turtlebot3 Waffle Pi to autonomously navigate through the manufacturing plant without collision using DQN algorithm and human intervention data for faster learning', 'Reinforcement Learning, ROS', 'https://github.com/arsalananwar11/Autonomous-Robot-Navigation-Using-DRL', 1, 'ros.jpg');"""
+	cursor.execute(query)
 
+	query = """INSERT INTO PROJECTS VALUES (3, 'Stopper Defect Detection in Production Line', 'Designed a scalable architecture to identify and classify defects on the elastomer stoppers using XceptionNet model with an accuracy of ~92%', 'Computer Vision, Deep Learning', 'https://github.com/arsalananwar11', 1, 'defectdetection.jpg');"""
+	cursor.execute(query)
+
+	query = """INSERT INTO PROJECTS VALUES (4, 'Customer Risk Assessment, Analysis and Management', 'Developed a system that identifies high value customers based on CLV who at risk of churning and flags them so that they can be incentivised for retention', 'Churn Analysis, CLV Modeling', 'https://github.com/arsalananwar11', 1, 'cra.jpg');"""
+	cursor.execute(query)
+
+	query = """INSERT INTO PROJECTS VALUES (5, 'Mum & Kin Food Services: An E-commerce platform', 'An E-commerce platform that allows people to order delicious and nutritious home-made food by connecting foodies with cooking enthusiasts', 'DBMS, Web Application', 'https://github.com/arsalananwar11', 0, 'mumkinfoods.jpg');"""
+	cursor.execute(query)
+	print('[SUCCESS] Query executed')
+
+	sqliteConnection.commit()
 	# Close the cursor
 	cursor.close()
 
